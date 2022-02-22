ad51a21e7f812eae80a82f0de21474779dfd4e77
Gustavo Coleta <gustavo.coleta@vrsoft.com.br>
2022-02-22 15:34:18 -0300

    First Commit

---
+++ app3/app.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+const host = '0.0.0.0';
+const port = process.env.PORT;
+const name = process.env.NAME;
+
+const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    res.writeHead(200);
+    res.end(`{"message": "${name} is running"}`);
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
+
